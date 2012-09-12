6aa8e21b5a095019747338921bfe43325c89396a
Peter <info@jetsli.de>
2012-09-12 11:45:58 +0200

    now on click uses correct coords; using astar

--- android-example/src/com/example/graphhopper/MainActivity.java
+++ android-example/src/com/example/graphhopper/MainActivity.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 import org.mapsforge.android.maps.MapActivity;
 import org.mapsforge.android.maps.MapView;
+import org.mapsforge.android.maps.Projection;
 import org.mapsforge.android.maps.overlay.ListOverlay;
 import org.mapsforge.android.maps.overlay.PolygonalChain;
 import org.mapsforge.android.maps.overlay.Polyline;
 import org.mapsforge.core.model.GeoPoint;
 import org.mapsforge.map.reader.header.FileOpenResult;
 
-import android.gesture.GestureOverlayView.OnGestureListener;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
@@ -22,17 +22,18 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
+import android.view.Window;
 import android.widget.Toast;
-import de.jetsli.graph.routing.DijkstraBidirection;
+import de.jetsli.graph.routing.AStar;
 import de.jetsli.graph.routing.Path;
+import de.jetsli.graph.routing.util.FastestCalc;
 import de.jetsli.graph.storage.Graph;
 import de.jetsli.graph.storage.Location2IDIndex;
 import de.jetsli.graph.storage.Location2IDQuadtree;
 import de.jetsli.graph.storage.MemoryGraphSafe;
 
 public class MainActivity extends MapActivity {
-	private static final GeoPoint BRANDENBURG_GATE = new GeoPoint(52.516273, 13.377725);
-	private static final GeoPoint VICTORY_COLUMN = new GeoPoint(52.514505, 13.350111);
+
 	private MapView mapView;
 	private Graph graph;
 	private Location2IDIndex locIndex;
@@ -43,10 +44,14 @@ public class MainActivity extends MapActivity {
 	ListOverlay pathOverlay = new ListOverlay();
 	SimpleOnGestureListener listener = new SimpleOnGestureListener() {
 
-		public boolean onSingleTapConfirmed(MotionEvent event) {
-			// TODO use the clicked points and not the center!
-			// event.getX(), event.getY()
-			GeoPoint tmp = mapView.getMapViewPosition().getMapPosition().geoPoint;
+		public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+			float x = motionEvent.getX();
+			float y = motionEvent.getY();
+			// byte zoomLevel = mapView.getMapViewPosition().getZoomLevel();
+
+			Projection p = mapView.getProjection();
+			GeoPoint tmp = p.fromPixels((int) x, (int) y);
+
 			if (start != null) {
 				calcPath(start.latitude, start.longitude, tmp.latitude, tmp.longitude);
 				start = null;
@@ -61,7 +66,9 @@ public class MainActivity extends MapActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
+
 		graph = createGraph();
 		locIndex = new Location2IDQuadtree(graph).prepareIndex(2000);
 		mapView = new MapView(this) {
@@ -82,7 +89,7 @@ public class MainActivity extends MapActivity {
 			finish();
 		}
 		setContentView(mapView);
-		
+
 		mapView.getOverlays().add(pathOverlay);
 	}
 
@@ -90,7 +97,8 @@ public class MainActivity extends MapActivity {
 		int locs = p.locations();
 		List<GeoPoint> geoPoints = new ArrayList<GeoPoint>(locs);
 		for (int i = 0; i < locs; i++) {
-			geoPoints.add(new GeoPoint(graph.getLatitude(p.location(i)), graph.getLongitude(p.location(i))));
+			geoPoints.add(new GeoPoint(graph.getLatitude(p.location(i)), graph
+					.getLongitude(p.location(i))));
 		}
 		PolygonalChain polygonalChain = new PolygonalChain(geoPoints);
 		Paint paintStroke = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -108,8 +116,8 @@ public class MainActivity extends MapActivity {
 		int toId = locIndex.findID(toLat, toLon);
 		log("found ids:" + fromId + "->" + toId + " via coords:" + fromLat + ","
 				+ fromLon + "->" + toLat + "," + toLon);
-		Path p = new DijkstraBidirection(graph).calcPath(fromId, toId);
-		log("found path:" + p.distance() + ", " + p.locations());		
+		Path p = new AStar(graph).setType(FastestCalc.DEFAULT).calcPath(fromId, toId);
+		log("found path:" + p.distance() + ", " + p.locations());
 
 		pathOverlay.getOverlayItems().clear();
 		pathOverlay.getOverlayItems().add(createPolyline(p));
